f47c56c348db2933fb53fee7412999b4ec4d8248
Marlon <marlonjames24@gmail.com>
2019-04-19 09:01:39 -0500

    Navigation Menu Update

--- projects/apple/js/main.js
+++ projects/apple/js/main.js
@@ -2,15 +2,18 @@
 //Variables
 var menuStack = document.querySelector('#menu');
 var navPhone = document.querySelector('#navPhone');
+var closeBtn = document.querySelector('#closebtn');
 navPhone.style.display = "none";
 
 
 //Event Listeners
 menuStack.addEventListener('click', openNav);
-navPhone.addEventListener('click', (e) =>{
-  navPhone.style.display = "none";
-});
+
+closeBtn.addEventListener('click', closeNav);
 //Open Navbar
 function openNav(){
     navPhone.style.display = "grid";
+}
+function closeNav(){
+  navPhone.style.display = "none";
 }
